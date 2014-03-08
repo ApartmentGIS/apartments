0884dd26c38aaea034f96848f8e10be67450eaa2
decemberliz <decemberliz922gmail.com>
2014-03-08 23:53:16 +0600

    added fields to admin

--- apartment_gis/app/admin.py
+++ apartment_gis/app/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from models import Apartment
 
-admin.site.register(Apartment)
+class ApartmentAdmin(admin.ModelAdmin):
+    list_display = ('address', 'district', 'rooms_num', 'month_price', 'floor')
+
+admin.site.register(Apartment, ApartmentAdmin)
--- apartment_gis/app/models.py
+++ apartment_gis/app/models.py
@@ -1,16 +1,20 @@
 from django.contrib.gis.db import models
+from django.contrib import admin
 
 class Apartment(models.Model):
-	address = models.CharField(max_length=200)
-	district = models.CharField(max_length=50)
-	rooms_num = models.IntegerField(default=1)
-	month_price = models.IntegerField()
-	floor = models.IntegerField()
-	storeys_num = models.IntegerField()
-	description = models.CharField(max_length=1000)
-	phone_number = models.CharField(max_length=50)
-	location = models.PointField()
-	objects = models.GeoManager()
-	
-	class Meta:
-		unique_together = ('address', 'rooms_num', 'month_price', 'floor',)
+    address = models.CharField(max_length=200)
+    district = models.CharField(max_length=50)
+    rooms_num = models.IntegerField(default=1)
+    month_price = models.IntegerField()
+    floor = models.IntegerField()
+    storeys_num = models.IntegerField()
+    description = models.CharField(max_length=1000)
+    phone_number = models.CharField(max_length=50)
+    location = models.PointField()
+    objects = models.GeoManager()
+
+    def __unicode__(self):
+        return self.address
+
+    class Meta:
+        unique_together = ('address', 'rooms_num', 'month_price', 'floor',)
