da229319cf9e814eb2371b5a8226041bd6237d2d
shpuntik <shpuntik74@gmail.com>
2014-03-08 15:45:01 +0600

    changeed filter form, added results grid

---
+++ apartment_gis/app/static/js/main.js
@@ -0,0 +1,44 @@
+$(function(){
+    $("#price-slider").noUiSlider({
+       range: [3000, 15000],
+       start: 3000,
+       step: 500,
+       connect: "lower",
+       handles: 1,
+       serialization: {
+           resolution: 1,
+           to: [
+               [$('#price-to'), 'html']
+           ]
+        }
+    });
+
+    $("#kindergarten-slider").noUiSlider({
+       range: [50, 1000],
+       start: 50,
+       step: 50,
+       connect: "lower",
+       handles: 1,
+       serialization: {
+           resolution: 1,
+           to: [
+               [$('#kindergarten-to'), 'html']
+           ]
+        }
+    });
+
+    var checkDisabledKindergartenSlider = function(){
+        if($("#kindergarten-check").is(":checked"))
+            $("#kindergarten-slider").removeAttr("disabled");
+        else
+            $("#kindergarten-slider").attr("disabled", "true");
+    };
+
+    checkDisabledKindergartenSlider();
+
+    $("#kindergarten-check").change(function(){
+        checkDisabledKindergartenSlider();
+    });
+
+
+});
