a20e9279cd7ee5e8bd9928b96e9ccb6ef150b604
decemberliz <decemberliz922gmail.com>
2014-03-09 21:14:49 +0600

    added nurseryschool model, configured admin page

--- apartment_gis/app/admin.py
+++ apartment_gis/app/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from models import Apartment
+from models import Apartment, NurserySchool
 
 class ApartmentAdmin(admin.ModelAdmin):
     list_display = ('address', 'district', 'rooms_num', 'month_price', 'floor')
 
+class NurserySchoolAdmin(admin.ModelAdmin):
+    lilst_display = ('name', 'address')
+
 admin.site.register(Apartment, ApartmentAdmin)
+admin.site.register(NurserySchool, NurserySchoolAdmin)
--- apartment_gis/app/models.py
+++ apartment_gis/app/models.py
@@ -18,3 +18,14 @@ class Apartment(models.Model):
 
     class Meta:
         unique_together = ('address', 'rooms_num', 'month_price', 'floor',)
+
+class NurserySchool(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=200)
+    phone_number = models.CharField(max_length=50)
+    location = models.PointField()
+    objects = models.GeoManager()
+
+
+    def __unicode__(self):
+        return self.name
