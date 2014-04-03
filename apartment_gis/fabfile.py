b8dcff4d5a0a68be7e339682bdcbd1d8c0f7da99
Alexandr Ivantsov <shpuntik74@gmail.com>
2014-04-03 22:44:09 +0600

    add command to prepare test instance

--- apartment_gis/fabfile.py
+++ apartment_gis/fabfile.py
@@ -1,12 +1,14 @@
 from fabric.api import *
 
+def prepare_test_instance():
+    local('ln -sf settings.py.production settings.py')
+
 def update_server():
     project_dir = '/home/ubuntu/apartments/apartment_gis/apartment_gis'
     with lcd(project_dir):
         local('git pull')
-        local('rm settings.py')
-        local('ln -s settings.py.production settings.py')
+        local('ln -sf settings.py.production settings.py')
         local('python ../manage.py syncdb')
-        local('sudo service postgresql restart')
-        local('sudo service nginx restart')
-        local('sudo supervisorctl restart all')
+        local('service postgresql restart')
+        local('service nginx restart')
+        local('supervisorctl restart all')
