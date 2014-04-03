28a2bedaf2acf0c0d77f58e851a25d1c83c5b3e0
Alexandr Ivantsov <shpuntik74@gmail.com>
2014-04-03 22:12:41 +0600

    Update fabfile.py

--- apartment_gis/fabfile.py
+++ apartment_gis/fabfile.py
@@ -1,10 +1,12 @@
-from fabric.api import local
+from fabric.api import *
 
 def update_server():
-    local('git pull')
-	local('rm settings.py')
-	local('ln -s settings.py.production settings.py')
-	local('python ../manage.py syncdb')
-	local('service postgresql restart')
-	local('service nginx restart')
-	local('supervisorctl restart all')
+    project_dir = '/home/ubuntu/apartments/apartment_gis/apartment_gis'
+    with lcd(project_dir):
+        local('git pull')
+        local('rm settings.py')
+        local('ln -s settings.py.production settings.py')
+        local('python ../manage.py syncdb')
+        local('sudo service postgresql restart')
+        local('sudo service nginx restart')
+        local('sudo supervisorctl restart all')
