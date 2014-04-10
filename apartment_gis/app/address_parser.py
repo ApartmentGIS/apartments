b9f83148f535eae6b65e73633bae57cee22ff651
decemberliz <decemberliz922gmail.com>
2014-04-10 13:18:54 +0600

    apartments parser code refactoring

--- apartment_gis/app/address_parser.py
+++ apartment_gis/app/address_parser.py
@@ -3,13 +3,14 @@ from django.utils.encoding import smart_str, smart_unicode
 from bs4 import BeautifulSoup as BS
 import urllib
 import csv
-import time
 import json
 import argparse
+
 
 class AptDataParser():
     def __init__(self):
         self.apt_parameters_list = []
+        self.cur_apt_number = 0
 
     def get_html_page(self, url):
         page_file = urllib.urlopen(url)
@@ -18,11 +19,11 @@ class AptDataParser():
         return page_html
 
     def is_already_added(self, address):
-        if (len(self.apt_parameters_list) == 0):
+        if len(self.apt_parameters_list) == 0:
             return False
         else:
             for i in xrange(0, len(self.apt_parameters_list)):
-                if (smart_str(address) == self.apt_parameters_list[i][0]):
+                if smart_str(address) == self.apt_parameters_list[i][0]:
                     return True
         return False
 
@@ -32,64 +33,81 @@ class AptDataParser():
         location = data['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['Point']['pos']
         return location
 
-    def parse_apt_data(self, classname, soup_object):
-        apt_list = soup_object.findAll('tr', {'class': classname})[::2]
-        k = 0
-        for apt in apt_list:
-            print '%s) %s' % (k, apt.a.b.string)
-            k += 1
-            address = apt.a.b.string
-            if ( self.is_already_added(address) == True):
-                continue
+    def get_address(self, apt_info):
+        address = apt_info.a.b.string
+        return address
 
-            district = apt.div.string
+    def get_district(self, apt_info):
+        return apt_info.div.string
 
-            basic_params = apt.findAll('td', {'class':'black'})
-            if ( len(basic_params[0].string.split())==1):
-                continue
+    def get_price(self, basic_params):
+        return int(basic_params[0].string.split()[0] + basic_params[0].string.split()[1])
 
-            price = int(basic_params[0].string.split()[0] + basic_params[0].string.split()[1])
-
-            if ( len(basic_params[2].string.split('-'))==1):
-                continue
-            rooms_num = int(basic_params[2].string.split('-')[0])
-            floor = int(basic_params[3].string.split('/')[0])
-            storeys_num = int(basic_params[3].string.split('/')[1])
-            details_url = apt.find('a', href=True)
-            details_soup = BS(''.join(self.get_html_page("http://domchel.ru" + details_url['href'])))
-            description = self.get_description(details_soup)
-            phone = self.get_phone(details_soup)
-            if (phone == None):
-                continue
-            location = self.get_location(address)
-            self.apt_parameters_list.append([smart_str(address), smart_str(district), rooms_num, price, floor, storeys_num, smart_str(description), phone, location])
-
-    def get_description(self, details_soup):
-        description = details_soup.findAll('ul', {'class':'review_right'})
-        if (len(description)==1 or len(description) == 0):
+    def get_rooms_num(self, basic_params):
+        if len(basic_params[2].string.split('-')) == 1:
             return None
         else:
-            return description[1].li.contents[0].split('\t')[1]
+            return int(basic_params[2].string.split('-')[0])
+
+    def get_description(self, details_soup):
+        #print type(details_soup)
+        description = details_soup.findAll('ul', {'class': 'review_right'})
+        if len(description) == 1 or len(description) == 0:
+            return None
+        else:
+            #print description[1].li.contents[0].replace('\t', '')
+            return description[1].li.contents[0].replace('\t', '')
 
     def get_phone(self, details_soup):
-        phone_number = details_soup.findAll('div', {'class':'rl_field'})
-        if ( len(phone_number)== 3 ):
-            return phone_number[0].contents[2].split(':')[1].replace(' ','')
-        if ( len(phone_number)== 5 ):
-            return phone_number[1].contents[2].split(':')[1].replace(' ','')
-        if( len(phone_number)== 4 or len(phone_number) == 0 ):
+        phone_number = details_soup.findAll('div', {'class': 'rl_info'})
+        try:
+            return phone_number[0].div.div.contents[2].split(':')[1].replace(' ', '')
+        except Exception as error:
+            print error
             return None
+
+    def parse_apt_data(self, classname, soup_object):
+        apt_list = soup_object.findAll('tr', {'class': classname})[::2]
+        for apt in apt_list:
+            address = self.get_address(apt)
+            if self.is_already_added(address):
+                continue
+
+            basic_params = apt.findAll('td', {'class': 'black'})
+            if len(basic_params[0].string.split()) == 1:
+                continue
+
+            rooms_num = self.get_rooms_num(basic_params)
+            if rooms_num is None:
+                continue
+
+            details_url = apt.find('a', href=True)
+            details_soup = BS(''.join(self.get_html_page("http://domchel.ru" + details_url['href'])))
+            phone = self.get_phone(details_soup)
+            if phone is None:
+                continue
+
+            district = self.get_district(apt)
+            price = self.get_price(basic_params)
+            floor = int(basic_params[3].string.split('/')[0])
+            storeys_num = int(basic_params[3].string.split('/')[1])
+            description = self.get_description(details_soup)
+            location = self.get_location(address)
+            print address
+            self.apt_parameters_list.append([smart_str(address), smart_str(district), rooms_num, price, floor, storeys_num, smart_str(description), phone, location])
+
+    def get_apartments_list(self):
+        return self.apt_parameters_list
 
     def write_in(self, filename, data_list):
         csvfile = open(filename, 'wb+')
         wr = csv.writer(csvfile, delimiter='#', quoting=csv.QUOTE_NONNUMERIC)
         for data_item in data_list:
-            print data_item[0]
             wr.writerow(data_item)
         csvfile.close()
 
-    def get_apt_data(self, apt_number):
-        for i in xrange(1,int(apt_number)):
+    def get_apt_data(self, pages_number):
+        for i in xrange(1, pages_number):
                 soup = BS(''.join(self.get_html_page("http://domchel.ru/realty/lease/residential/secondary/#" + str(i) + ".php%order=DateUpdate&dir=desc&PriceUnit=1&AreaUnit=1&expand=0&PriceUnit=1")))
                 self.parse_apt_data('even', soup)
                 self.parse_apt_data('odd', soup)
@@ -119,19 +137,20 @@ class NurserySchoolDataParser(AptDataParser):
 if __name__ == '__main__':
     arg_parser = argparse.ArgumentParser(description='This program get list of apartments and schools information')
     arg_parser.add_argument('-apt_filename', '--apt_filename', help="Input file name with csv extention to get apartments info. Don't forget to specify number of apartments")
-    arg_parser.add_argument('-apt_number', '--apt_number', help="Input number of apartments you would like to add. Don't forget to specify filename for apartments information")
+    arg_parser.add_argument('-pages_number', '--pages_number', help="Input number of web pages you would like to parse. Don't forget to specify filename for apartments information")
     arg_parser.add_argument('-school_filename', '--school_filename', help='Input file name with csv extention to get schools info')
     args = vars(arg_parser.parse_args())
 
-    if not (args['school_filename'] or args['apt_filename'] or args['apt_number']):
+    if not (args['school_filename'] or args['apt_filename'] or args['pages_number']):
         arg_parser.print_help()
-        exit(0)
+        exit(1)
 
-    if(args['apt_filename'] and args['apt_number']):
+    if args['apt_filename'] and args['pages_number']:
             apt_parser = AptDataParser()
-            apt_parser.get_apt_data(args['apt_number'])
-            print "\n Total Number of Added Apartments: %s" % len(apt_parser.apt_parameters_list)
-            apt_parser.write_in(args['apt_filename'], apt_parser.apt_parameters_list)
+            apt_parser.get_apt_data(int(args['pages_number']))
+            apartments_list = apt_parser.get_apartments_list()
+            print "\nTotal Number of Added Apartments: %s" % len(apartments_list)
+            apt_parser.write_in(args['apt_filename'], apartments_list)
     elif(args['school_filename']):
         school_parser = NurserySchoolDataParser()
         data_list = school_parser.get_school_data()
@@ -140,4 +159,4 @@ if __name__ == '__main__':
     else:
         print "Error: Add apt_filename or apt_number \n"
         arg_parser.print_help()
-        exit(0)
+        exit(1)
