a8f834648b7a46a7c44403c5c9b3b785cf6e1852
decemberliz <decemberliz922gmail.com>
2014-04-12 19:52:41 +0600

    convert tabs to spaces

--- apartment_gis/app/forms.py
+++ apartment_gis/app/forms.py
@@ -3,46 +3,46 @@ import floppyforms as forms
 
 
 DISTRICT_CHOICES = (
-	('all', u'все'),
-	('metal', u'Металлургический'),
-	('szapa', u'Северо-запад'),
-	('svost', u'Северо-восток'),
-	('trakt', u'Тракторозаводской'),
-	('centr', u'Центральный'),
-	('sovet', u'Советский'),
-	('lenin', u'Ленинский')
+    ('all', u'все'),
+    ('metal', u'Металлургический'),
+    ('szapa', u'Северо-запад'),
+    ('svost', u'Северо-восток'),
+    ('trakt', u'Тракторозаводской'),
+    ('centr', u'Центральный'),
+    ('sovet', u'Советский'),
+    ('lenin', u'Ленинский')
 )
 
 ROOMS_NUM_CHOICES = (
-	('all', u'все'),
-	('1', '1'),
-	('2', '2'),
-	('big', 'большие')
+    ('all', u'все'),
+    ('1', '1'),
+    ('2', '2'),
+    ('big', 'большие')
 )
 
 
 class BtnCheckboxSelectMultiple(forms.CheckboxSelectMultiple):
-	template_name = 'floppyforms/btn_checkbox_select_multiple.html'
+    template_name = 'floppyforms/btn_checkbox_select_multiple.html'
 
 
 class CustomRangeInput(forms.RangeInput):
-	template_name = 'floppyforms/custom_range_input.html'
+    template_name = 'floppyforms/custom_range_input.html'
 
 
 class FilterForm(forms.Form):
-	district = forms.MultipleChoiceField(label=u'Район', widget=BtnCheckboxSelectMultiple(attrs={'class': 'districts'}), choices=DISTRICT_CHOICES)
-	rooms_num = forms.MultipleChoiceField(label=u'Количество комнат', widget=BtnCheckboxSelectMultiple(attrs={'class': 'rooms_nums'}), choices=ROOMS_NUM_CHOICES)
-	month_price = forms.IntegerField(label=u'Цена за месяц', widget=CustomRangeInput(attrs={
-		'id': 'price-max',
-		'min': 6000,
-		'max': 50000,
-		'step': 1000,
-		'measure': u'рублей'
-	}))
-	kindergarten_distance = forms.IntegerField(label=u'Детский сад', widget=CustomRangeInput(attrs={
-		'id': 'kindergarten',
-		'min': 300,
-		'max': 3000,
-		'step': 100,
-		'measure': u'метров'
-	}))
+    district = forms.MultipleChoiceField(label=u'Район', widget=BtnCheckboxSelectMultiple(attrs={'class': 'districts'}), choices=DISTRICT_CHOICES)
+    rooms_num = forms.MultipleChoiceField(label=u'Количество комнат', widget=BtnCheckboxSelectMultiple(attrs={'class': 'rooms_nums'}), choices=ROOMS_NUM_CHOICES)
+    month_price = forms.IntegerField(label=u'Цена за месяц', widget=CustomRangeInput(attrs={
+        'id': 'price-max',
+        'min': 6000,
+        'max': 50000,
+        'step': 1000,
+        'measure': u'рублей'
+    }))
+    kindergarten_distance = forms.IntegerField(label=u'Детский сад', widget=CustomRangeInput(attrs={
+        'id': 'kindergarten',
+        'min': 300,
+        'max': 3000,
+        'step': 100,
+        'measure': u'метров'
+    }))
--- apartment_gis/app/views.py
+++ apartment_gis/app/views.py
@@ -7,58 +7,58 @@ from forms import FilterForm
 
 
 DISTRICTS = {
-	'metal': u'Металлургический',
-	'szapa': u'Северо-запад',
-	'svost': u'Северо-восток',
-	'trakt': u'Тракторозаводской',
-	'centr': u'Центральный',
-	'sovet': u'Советский',
-	'lenin': u'Ленинский'
+    'metal': u'Металлургический',
+    'szapa': u'Северо-запад',
+    'svost': u'Северо-восток',
+    'trakt': u'Тракторозаводской',
+    'centr': u'Центральный',
+    'sovet': u'Советский',
+    'lenin': u'Ленинский'
 }
 
 def home(request):
-	filter = {
-		'district': ['all'],
-		'rooms_num': ['all'],
-		'month_price': 50000,
-		'kindergarten_distance': 3000
-	}
+    filter = {
+        'district': ['all'],
+        'rooms_num': ['all'],
+        'month_price': 50000,
+        'kindergarten_distance': 3000
+    }
 
-	if request.method == 'GET':
-		if ('district' in request.GET) and (request.GET['district'] is not None):
-			filter['district'] = request.GET.getlist('district')
-		if ('rooms_num' in request.GET) and (request.GET['rooms_num'] is not None):
-			filter['rooms_num'] = request.GET.getlist('rooms_num')
-		if ('month_price' in request.GET) and (request.GET['month_price'] is not None):
-			filter['month_price'] = request.GET['month_price']
-		if ('kindergarten_distance' in request.GET) and (request.GET['kindergarten_distance'] is not None):
-			filter['kindergarten_distance'] = request.GET['kindergarten_distance']
+    if request.method == 'GET':
+        if ('district' in request.GET) and (request.GET['district'] is not None):
+            filter['district'] = request.GET.getlist('district')
+        if ('rooms_num' in request.GET) and (request.GET['rooms_num'] is not None):
+            filter['rooms_num'] = request.GET.getlist('rooms_num')
+        if ('month_price' in request.GET) and (request.GET['month_price'] is not None):
+            filter['month_price'] = request.GET['month_price']
+        if ('kindergarten_distance' in request.GET) and (request.GET['kindergarten_distance'] is not None):
+            filter['kindergarten_distance'] = request.GET['kindergarten_distance']
 
-	args = []
-	kwargs = {}
+    args = []
+    kwargs = {}
 
-	if filter['district'][0] != 'all':
-		districts = []
-		for d in filter['district']:
-			districts.append(Q(district__contains=DISTRICTS[d]))
-		args.append(reduce(operator.or_, districts))
+    if filter['district'][0] != 'all':
+        districts = []
+        for d in filter['district']:
+            districts.append(Q(district__contains=DISTRICTS[d]))
+        args.append(reduce(operator.or_, districts))
 
-	if filter['rooms_num'][0] != 'all':
-		rooms = []
-		for r in filter['rooms_num']:
-			if r == '1' or r == '2':
-				rooms.append(Q(rooms_num=r))
-			elif r == 'big':
-				rooms.append(Q(rooms_num__gte=3))
+    if filter['rooms_num'][0] != 'all':
+        rooms = []
+        for r in filter['rooms_num']:
+            if r == '1' or r == '2':
+                rooms.append(Q(rooms_num=r))
+            elif r == 'big':
+                rooms.append(Q(rooms_num__gte=3))
 
-		args.append(reduce(operator.or_, rooms))
+        args.append(reduce(operator.or_, rooms))
 
-	kwargs['month_price__lte'] = filter['month_price']
+    kwargs['month_price__lte'] = filter['month_price']
 
-	apartment_list = Apartment.objects.filter(*args, **kwargs)
-	filter_form = FilterForm(filter)
+    apartment_list = Apartment.objects.filter(*args, **kwargs)
+    filter_form = FilterForm(filter)
 
-	return render(request, 'index.html', {
-		'apartment_list': apartment_list,
-		'filter_form': filter_form
-	})
+    return render(request, 'index.html', {
+        'apartment_list': apartment_list,
+        'filter_form': filter_form
+    })
