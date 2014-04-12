02417258462b8483c2512192231b16765664e0c9
shpuntik <shpuntik74@gmail.com>
2014-04-12 21:25:45 +0600

    update fabric file

--- apartment_gis/fabfile.py
+++ apartment_gis/fabfile.py
@@ -12,10 +12,19 @@ def start_test():
 
 def update_server():
     project_dir = '/home/ubuntu/apartments/apartment_gis/apartment_gis'
+
+    local('rm -f /home/ubuntu/apartments/ -r')
+
+    with lcd('/home/ubuntu/'):
+        local('git clone https://github.com/ApartmentGIS/apartments.git')
+
+    local('chmod a+w -R /home/ubuntu/apartments/')
+
     with lcd(project_dir):
-        local('git pull')
+        local('mkdir ../logs')
         local('ln -sf settings.py.production settings.py')
         local('python ../manage.py migrate app')
         local('service postgresql restart')
         local('service nginx restart')
         local('supervisorctl restart all')
+
