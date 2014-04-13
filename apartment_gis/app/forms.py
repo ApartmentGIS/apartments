f97e4a13aa81959a59c6918d11dc2e6c8edc3c23
shpuntik <shpuntik74@gmail.com>
2014-04-13 11:33:31 +0600

    update interface and search logic for organizations

--- apartment_gis/app/forms.py
+++ apartment_gis/app/forms.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
+from django.db.models import Count, Max, Min
 import floppyforms as forms
+from models import Apartment
 
 
 DISTRICT_CHOICES = (
@@ -13,14 +15,6 @@ DISTRICT_CHOICES = (
     ('lenin', u'Ленинский')
 )
 
-ROOMS_NUM_CHOICES = (
-    ('all', u'все'),
-    ('1', '1'),
-    ('2', '2'),
-    ('big', 'большие')
-)
-
-
 class BtnCheckboxSelectMultiple(forms.CheckboxSelectMultiple):
     template_name = 'floppyforms/btn_checkbox_select_multiple.html'
 
@@ -29,18 +23,80 @@ class CustomRangeInput(forms.RangeInput):
     template_name = 'floppyforms/custom_range_input.html'
 
 
+class LabelCheckbox(forms.CheckboxInput):
+    template_name = 'floppyforms/label_checkbox.html'
+
+
+def getRoomNumChoices():
+    rooms_num_db = Apartment.objects.values('rooms_num').annotate(rooms_count=Count('rooms_num')).order_by('rooms_num')
+    rooms_num_choices = [('all', u'все')]
+    for room in rooms_num_db:
+        rooms_num_choices.append((room['rooms_num'], room['rooms_num']))
+
+    return rooms_num_choices
+
+
+def getMinMonthPrice():
+    return Apartment.objects.all().aggregate(Min('month_price'))['month_price__min']
+
+
+def getMaxMonthPrice():
+    return Apartment.objects.all().aggregate(Max('month_price'))['month_price__max']
+
+
 class FilterForm(forms.Form):
     district = forms.MultipleChoiceField(label=u'Район', widget=BtnCheckboxSelectMultiple(attrs={'class': 'districts'}), choices=DISTRICT_CHOICES)
-    rooms_num = forms.MultipleChoiceField(label=u'Количество комнат', widget=BtnCheckboxSelectMultiple(attrs={'class': 'rooms_nums'}), choices=ROOMS_NUM_CHOICES)
+    rooms_num = forms.MultipleChoiceField(label=u'Количество комнат', widget=BtnCheckboxSelectMultiple(attrs={'class': 'rooms_nums'}), choices=getRoomNumChoices())
     month_price = forms.IntegerField(label=u'Цена за месяц', widget=CustomRangeInput(attrs={
         'id': 'price-max',
-        'min': 6000,
-        'max': 50000,
+        'min': getMinMonthPrice(),
+        'max': getMaxMonthPrice(),
         'step': 1000,
         'measure': u'рублей'
     }))
-    kindergarten_distance = forms.IntegerField(label=u'Детский сад', widget=CustomRangeInput(attrs={
+    fitnessclub_checkbox = forms.BooleanField(label='', widget=LabelCheckbox(attrs={'label': u'Фитнес центр'}), required=False)
+    fitnessclub_distance = forms.IntegerField(label='', widget=CustomRangeInput(attrs={
+        'id': 'fitnessclub',
+        'min': 300,
+        'max': 3000,
+        'step': 100,
+        'measure': u'метров'
+    }))
+    hospital_checkbox = forms.BooleanField(label='', widget=LabelCheckbox(attrs={'label': u'Больница'}), required=False)
+    hospital_distance = forms.IntegerField(label='', widget=CustomRangeInput(attrs={
+        'id': 'hospital',
+        'min': 300,
+        'max': 3000,
+        'step': 100,
+        'measure': u'метров'
+    }))
+    kindergarten_checkbox = forms.BooleanField(label='', widget=LabelCheckbox(attrs={'label': u'Детский сад'}), required=False)
+    kindergarten_distance = forms.IntegerField(label='', widget=CustomRangeInput(attrs={
         'id': 'kindergarten',
+        'min': 300,
+        'max': 3000,
+        'step': 100,
+        'measure': u'метров'
+    }))
+    school_checkbox = forms.BooleanField(label='', widget=LabelCheckbox(attrs={'label': u'Школа'}), required=False)
+    school_distance = forms.IntegerField(label='', widget=CustomRangeInput(attrs={
+        'id': 'school',
+        'min': 300,
+        'max': 3000,
+        'step': 100,
+        'measure': u'метров'
+    }))
+    university_checkbox = forms.BooleanField(label='', widget=LabelCheckbox(attrs={'label': u'Университет'}), required=False)
+    university_distance = forms.IntegerField(label='', widget=CustomRangeInput(attrs={
+        'id': 'university',
+        'min': 300,
+        'max': 3000,
+        'step': 100,
+        'measure': u'метров'
+    }))
+    shopmall_checkbox = forms.BooleanField(label='', widget=LabelCheckbox(attrs={'label': u'Торговый центр'}), required=False)
+    shopmall_distance = forms.IntegerField(label='', widget=CustomRangeInput(attrs={
+        'id': 'shopmall',
         'min': 300,
         'max': 3000,
         'step': 100,
--- apartment_gis/app/views.py
+++ apartment_gis/app/views.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
 from django.db.models import Q
+from django.contrib.gis.measure import D
 import operator
-from models import Apartment
+from models import Apartment, Organization
 from forms import FilterForm
 
 
@@ -16,13 +17,36 @@ DISTRICTS = {
     'lenin': u'Ленинский'
 }
 
+ORGANIZATIONS = (
+    ('kindergarten', 'KIN'),
+    ('school', 'SCH'),
+    ('university', 'UNI'),
+    ('hospital', 'HOS'),
+    ('fitnessclub', 'FIT'),
+    ('shopmall', 'SHP')
+)
+
+
 def home(request):
     filter = {
         'district': ['all'],
         'rooms_num': ['all'],
         'month_price': 50000,
-        'kindergarten_distance': 3000
+        # 'kindergarten_checkbox': '',
+        'kindergarten_distance': 3000,
+        # 'school_checkbox': '',
+        'school_distance': 3000,
+        # 'university_checkbox': '',
+        'university_distance': 3000,
+        # 'hospital_checkbox': '',
+        'hospital_distance': 3000,
+        # 'fitnessclub_checkbox': '',
+        'fitnessclub_distance': 3000,
+        # 'shopmall_checkbox': '',
+        'shopmall_distance': 3000
     }
+
+    check_organizations = []
 
     if request.method == 'GET':
         if ('district' in request.GET) and (request.GET['district'] is not None):
@@ -31,8 +55,13 @@ def home(request):
             filter['rooms_num'] = request.GET.getlist('rooms_num')
         if ('month_price' in request.GET) and (request.GET['month_price'] is not None):
             filter['month_price'] = request.GET['month_price']
-        if ('kindergarten_distance' in request.GET) and (request.GET['kindergarten_distance'] is not None):
-            filter['kindergarten_distance'] = request.GET['kindergarten_distance']
+
+        for organization in ORGANIZATIONS:
+            if (organization[0] + '_checkbox' in request.GET) and (request.GET[organization[0] + '_checkbox'] is not None):
+                filter[organization[0] + '_checkbox'] = request.GET[organization[0] + '_checkbox']
+                check_organizations.append(organization)
+                if (organization[0] + '_distance' in request.GET) and (request.GET[organization[0] + '_distance'] is not None):
+                    filter[organization[0] + '_distance'] = request.GET[organization[0] + '_distance']
 
     args = []
     kwargs = {}
@@ -46,19 +75,28 @@ def home(request):
     if filter['rooms_num'][0] != 'all':
         rooms = []
         for r in filter['rooms_num']:
-            if r == '1' or r == '2':
-                rooms.append(Q(rooms_num=r))
-            elif r == 'big':
-                rooms.append(Q(rooms_num__gte=3))
+            rooms.append(Q(rooms_num=r))
 
         args.append(reduce(operator.or_, rooms))
 
     kwargs['month_price__lte'] = filter['month_price']
 
     apartment_list = Apartment.objects.filter(*args, **kwargs)
+    apartment_list_with_organizations = apartment_list
+
+    for organization in check_organizations:
+        apartment_list_for_organization = []
+
+        for department in Organization.objects.filter(type=organization[1]):
+            good_variants = apartment_list.filter(location__distance_lte=(department.location, D(m=filter[organization[0] + '_distance'])))
+            if len(good_variants) != 0:
+                apartment_list_for_organization = list(set(apartment_list_for_organization) | set(good_variants))
+
+        apartment_list_with_organizations = list(set(apartment_list_with_organizations) & set(apartment_list_for_organization))
+
     filter_form = FilterForm(filter)
 
     return render(request, 'index.html', {
-        'apartment_list': apartment_list,
+        'apartment_list': apartment_list_with_organizations,
         'filter_form': filter_form
     })
